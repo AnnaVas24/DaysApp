c1a413971f27fc965e269308bc77cb49e3edd7c5
Anna <anna.vasi4ko@icloud.com>
2022-06-25 22:11:31 +0300

    Initial Commit

---
+++ DaysApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DaysApp
+//
+//  Created by Vasichko Anna on 25.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
