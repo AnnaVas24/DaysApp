8e8b319071bcd3bf8ead4b94be0b9b8d75d8a9fa
Anna <anna.vasi4ko@icloud.com>
2022-06-27 00:12:38 +0300

    days app

--- DaysApp/ViewController.swift
+++ DaysApp/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var infoLabel: UILabel!
+    
+    private var numberOfDays = 0
+   
+    
+    @IBAction func datePicker(_ sender: UIDatePicker) {
+        let calendar = Calendar.current
+        
+        let currentDate = calendar.startOfDay(for: Date.now)
+        let dateOfBirth = calendar.startOfDay(for: sender.date)
+        
+        let days = calendar.dateComponents([.day], from: dateOfBirth, to: currentDate)
+        numberOfDays = days.day ?? 0
     }
-
-
+    
+    @IBAction func resultButtonPressed(_ sender: UIButton) {
+        infoLabel.text = "You have enjoyed your life for \(numberOfDays) days"
+    }
+    
 }
 
